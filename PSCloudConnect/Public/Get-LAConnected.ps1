d0d31d7b2160c4f8fa313fa6c3e89f5b943bc426
kevinblumenfeld <github@thenext.net>
2017-07-18 22:41:30 -0400

    updated help and added support for multiple users in a single tenant (with -user parameter)

--- PSCloudConnect/Public/Get-LAConnected.ps1
+++ PSCloudConnect/Public/Get-LAConnected.ps1
@@ -7,8 +7,11 @@ function Get-LAConnected {
     Param
     (
 
-        [parameter(Mandatory = $true)]
+        [parameter(Position = 0, Mandatory = $true)]
         [string] $Tenant,
+
+        [parameter(Position = 1, Mandatory = $false)]
+        [string] $User,
                            
         [Parameter(Mandatory = $false)]
         [switch] $Exchange,
@@ -46,6 +49,9 @@ function Get-LAConnected {
         if ($Tenant -match 'onmicrosoft') {
             $Tenant = $Tenant.Split(".")[0]
         }
+        if (! $User) {
+            $User = "Default"
+        }
     }
     Process {
 
@@ -54,16 +60,20 @@ function Get-LAConnected {
 
         # Delete invalid or unwanted credentials
         if ($Delete365Creds) {
-            Remove-Item ($KeyPath + "$($Tenant).cred") 
-            Remove-Item ($KeyPath + "$($Tenant).ucred")
+            Remove-Item ($KeyPath + "$($Tenant).$($user).cred") 
+            Remove-Item ($KeyPath + "$($Tenant).$($user).ucred")
         }
         # Create Directory for Transact Logs
         if (!(Test-Path ($RootPath + $Tenant + "\logs\"))) {
             New-Item -ItemType Directory -Force -Path ($RootPath + $Tenant + "\logs\")
         }
-
-        Start-Transcript -path ($RootPath + $Tenant + "\logs\" + "transcript-" + ($(get-date -Format _yyyy-MM-dd_HH-mm-ss)) + ".txt")
-
+        Try {
+            Start-Transcript -ErrorAction Stop -path ($RootPath + $Tenant + "\logs\" + "transcript-" + ($(get-date -Format _yyyy-MM-dd_HH-mm-ss)) + ".txt") 
+        }
+        Catch {
+            Stop-Transcript 
+            Start-Transcript -path ($RootPath + $Tenant + "\logs\" + "transcript-" + ($(get-date -Format _yyyy-MM-dd_HH-mm-ss)) + ".txt")
+        }
         # Create KeyPath Directory
         if (!(Test-Path $KeyPath)) {
             try {
@@ -74,20 +84,27 @@ function Get-LAConnected {
             }           
         }
         if ($Exchange -or $MSOnline -or $All365 -or $Skype -or $SharePoint -or $Compliance -or $AzureADver2) {
-            if (Test-Path ($KeyPath + "$($Tenant).cred")) {
-                $PwdSecureString = Get-Content ($KeyPath + "$($Tenant).cred") | ConvertTo-SecureString
-                $UsernameString = Get-Content ($KeyPath + "$($Tenant).ucred") 
+            if (Test-Path ($KeyPath + "$($Tenant).$($user).cred")) {
+                $PwdSecureString = Get-Content ($KeyPath + "$($Tenant).$($user).cred") | ConvertTo-SecureString
+                $UsernameString = Get-Content ($KeyPath + "$($Tenant).$($user).ucred")
                 $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $UsernameString, $PwdSecureString 
             }
             else {
                 $Credential = Get-Credential -Message "Enter a username and password"
-                $Credential.Password | ConvertFrom-SecureString | Out-File "$($KeyPath)\$Tenant.cred" -Force
-                $Credential.UserName | Out-File "$($KeyPath)\$Tenant.ucred"
+                $Credential.Password | ConvertFrom-SecureString | Out-File ($KeyPath + "$($Tenant).$($user).cred") -Force
+                $Credential.UserName | Out-File ($KeyPath + "$($Tenant).$($user).ucred")
             }
         }
         if ($MSOnline -or $All365) {
             # Office 365 Tenant
-            Import-Module MsOnline
+            Try {
+                Import-Module MsOnline -ErrorAction Stop
+            }
+            Catch {
+                Write-Output "MSOnline module is required"
+                Write-Output "To download the prerequisite and MSOnline module:"
+                Write-Output "https://technet.microsoft.com/en-us/library/dn975125.aspx"
+            }
             Connect-MsolService -Credential $Credential
         }
         if ($Exchange -or $All365) {
@@ -97,7 +114,14 @@ function Get-LAConnected {
                 Import-Module (Import-PSSession $exchangeSession -AllowClobber) -Global | Out-Null
             }
             else {
-                Connect-EXOPSSession -UserPrincipalName $Credential.UserName
+                Try {
+                    Connect-EXOPSSession -UserPrincipalName $Credential.UserName -erroraction Stop
+                } 
+                Catch [System.Management.Automation.CommandNotFoundException] {
+                    Write-Output "Exchange Online MFA module is required"
+                    Write-Output "To download the Exchange Online Remote PowerShell Module for multi-factor authentication,"
+                    Write-Output "in the EAC (https://outlook.office365.com/ecp/), go to Hybrid > Setup and click the appropriate Configure button."
+                }
             }
         }
         # Security and Compliance Center
@@ -108,16 +132,40 @@ function Get-LAConnected {
         # Skype Online
         if ($Skype -or $All365) {
             if (! $MFA) {
-                $sfboSession = New-CsOnlineSession -Credential $Credential
+                Try {
+                    $sfboSession = New-CsOnlineSession -ErrorAction Stop -Credential $Credential
+                }
+                Catch [System.Management.Automation.CommandNotFoundException] {
+                    Write-Output "Skype for Business Online Module not found.  Please download and install it from here:"
+                    Write-Output "https://www.microsoft.com/en-us/download/details.aspx?id=39366"
+                }
+                Catch {
+                    $_
+                }
                 Import-Module (Import-PSSession $sfboSession -AllowClobber) -Global | Out-Null
             }
             else {
-                $sfboSession = New-CsOnlineSession -UserName $Credential.UserName
+                Try {
+                    $sfboSession = New-CsOnlineSession -UserName $Credential.UserName -ErrorAction Stop
+                }
+                Catch [System.Management.Automation.CommandNotFoundException] {
+                    Write-Output "Skype for Business Online Module not found.  Please download and install it from here:"
+                    Write-Output "https://www.microsoft.com/en-us/download/details.aspx?id=39366"
+                }
+                Catch {
+                    $_
+                }
             }
         }
         # SharePoint Online
         if ($SharePoint -or $All365) {
-            Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking 
+            Try {
+                Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking -ErrorAction Stop
+            }
+            Catch {
+                Write-Output "Unable to import SharePoint Module"
+                Write-Output "Ensure it is installed, Download it from here: https://www.microsoft.com/en-us/download/details.aspx?id=35588"
+            }
             if (! $MFA) {
                 Connect-SPOService -Url ("https://" + $Tenant + "-admin.sharepoint.com") -credential $Credential
             }
@@ -131,9 +179,30 @@ function Get-LAConnected {
         }
         # Azure AD
         If ($AzureADver2) {
-            install-module azuread -AllowClobber -Force
-            import-module azuread -Force
-            Connect-AzureAD -Credential $Credential
+            if (! $MFA) {
+                Try {
+                    install-module azuread -AllowClobber -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                    import-module azuread -Force -ErrorAction Stop
+                    Connect-AzureAD -Credential $Credential -ErrorAction Stop
+                }
+                Catch {
+                    Write-Output "There was an error Connecting to Azure Ad - Ensure the module is installed"
+                    Write-Output "Download PowerShell 5 or PowerShellGet"
+                    Write-Output "https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure"
+                }
+            }
+            else {
+                Try {
+                    install-module azuread -AllowClobber -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+                    import-module azuread -Force -ErrorAction Stop
+                    Connect-AzureAD -ErrorAction Stop
+                }
+                Catch {
+                    Write-Output "There was an error Connecting to Azure Ad - Ensure the module is installed"
+                    Write-Output "Download PowerShell 5 or PowerShellGet"
+                    Write-Output "https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure"
+                }
+            }
         }
     }
     End {
@@ -153,7 +222,16 @@ function Get-LAAzureConnected {
             $json = $json | select name | Out-GridView -PassThru -Title "Select Azure username or click Cancel to use another"
         }
         if (!($json)) {
-            $azlogin = Login-AzureRmAccount
+            Try {
+                install-module AzureRM -Force -erroraction continue
+                $azlogin = Login-AzureRmAccount -erroraction continue
+            }
+            catch [System.Management.Automation.CommandNotFoundException] {
+                Write-Output "Download and install PowerShell 5.1 or PowerShellGet so the AzureRM module can be automatically installed"
+                Write-Output "https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.2.0#how-to-get-powershellget"
+                Write-Output "or download the MSI installer and install from here: https://github.com/Azure/azure-powershell/releases"
+                Break
+            }
             Save-AzureRmContext -Path ($KeyPath + ($azlogin.Context.Account.Id) + ".json")
             Import-AzureRmContext -Path ($KeyPath + ($azlogin.Context.Account.Id) + ".json")
         }
@@ -175,13 +253,21 @@ function Get-LAAzureConnected {
             Write-Output "   Azure credentials have expired. Authenticate again please."
             Write-Host   "*********************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
             Write-Host   "*********************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
-        
+            
             Remove-Item ($KeyPath + $json.name)
             Get-LAAzureConnected
         }
     }
     else {
-        Login-AzureRmAccount
+        Try {
+            Login-AzureRmAccount -ErrorAction Stop
+        }
+        catch [System.Management.Automation.CommandNotFoundException] {
+            Write-Output "Download and install PowerShell 5.1 or PowerShellGet so the AzureRM module can be automatically installed"
+            Write-Output "https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.2.0#how-to-get-powershellget"
+            Write-Output "or download the MSI installer and install from here: https://github.com/Azure/azure-powershell/releases"
+            Break
+        }
         Write-Host   "*********************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
         Write-Host   "*********************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
         Write-Output "   Select Subscription and Click `"OK`" in lower right-hand corner"
--- PSCloudConnect/docs/Get-LAConnected.md
+++ PSCloudConnect/docs/Get-LAConnected.md
@@ -18,8 +18,8 @@ Update-Module PSCloudConnect
 ## SYNTAX
 
 ```
-Get-LAConnected [-Tenant] <String> [-Exchange] [-MSOnline] [-All365] [-Azure] [-Skype] [-SharePoint]
- [-Compliance] [-AzureADver2] [-MFA] [-Delete365Creds] [-WhatIf] [-Confirm] [<CommonParameters>]
+Get-LAConnected [-Tenant] <String> [-User <String>] [-Exchange] [-MSOnline] [-All365] [-Azure] [-Skype]
+ [-SharePoint] [-Compliance] [-AzureADver2] [-MFA] [-Delete365Creds] [-WhatIf] [-Confirm] [<CommonParameters>]
 ```
 
 ## DESCRIPTION
@@ -27,22 +27,26 @@ Connects to Office 365 services and/or Azure.
 
 Connects to some or all of the Office 365/Azure services based on switches provided at runtime.
 
-Office 365 tenant name, for example, either contoso or contoso.onmicrosoft.com must be provided with -Tenant parameter
-When connecting to just Azure, it is still required to provide a unique name for the -Tenant parameter.
+Office 365 tenant name, for example, either contoso or contoso.onmicrosoft.com must be provided with -Tenant parameter.
+Additionally, if more than one username will be used against a single tenant, use the -User parameter (for the second username and on).  Use anything unique to that username so the credential can be properly saved.  Both the -Tenant and -User parameters are positional so it's not necessary to type -Tenant or -User.  The -Tenant parameter is mandatory while the -User parameter is optional.
+
+For example, _Get-LaConnected Contoso -Exchange_ demonstrates how it is not necessary to use the -User parameter and how the -Tenant parameter is positional.  However, say I want to connect to the Contoso tenant as _frank@contoso.com_  I could use _Get-LaConnected Contoso Frank -Exchange_
+
+When just connecting to Azure, it is still required to provide a Tenant, anything that uniquely identifies it.
 
 There is a switch to use Multi-Factor Authentication.  For Exchange Online MFA, you are required to download and use the Exchange Online Remote PowerShell Module.
 To download the Exchange Online Remote PowerShell Module for multi-factor authentication, in the EAC (https://outlook.office365.com/ecp/), go to Hybrid > Setup and click the appropriate Configure button.  When using Multi-Factor Authentication the saving of credentials is not available currently - thus each service will prompt independently for credentials.  Also the Security and Compliance Center does not currently support multi-factor authentication.
 
 Locally saves and encrypts to a file the username and password.
-The encrypted file...can only be used on the computer and within the user's profile from which it was created, is the same .txt file for all the Office 365 services and is a separate .json file for Azure.
+The encrypted file...can only be used on the computer and within the user's profile from which it was created, is the same .txt file for all the Office 365 services and is a separate .json file for Azure.  If a username or password becomes corrupt or is entered incorrectly, it can be deleted using -Delete365Creds.  For example, _Get-LaConnected Contoso -Delete365Creds_
 
-If Azure or AzureOnly switch is used **for first time**:
+If Azure switch is used **for first time**:
 
 1.  User will login as normal when prompted by Azure
 2.  User will be prompted to select which Azure Subscription
 3.  Select the subscription and click "OK"
 
-If Azure or AzureOnly switch is used:
+If Azure switch is used after first time:
 
 1.  User will be prompted to pick username used previously
 2.  If a new username is to be used (e.g. username not found when prompted), click **Cancel** to be prompted to login.
@@ -70,12 +74,12 @@ The tenant must be specified, for example either contoso or contoso.onmicrosoft.
 
 ### -------------------------- EXAMPLE 2 --------------------------
 ```
-Get-LAConnected -Tenant Contoso -Skype -Azure -Exchange -MSOnline
+Get-LAConnected Contoso -Skype -Azure -Exchange -MSOnline
 ```
 
 Connects to Azure, MS Online Service (MSOL), Exchange Online & Skype
 
-This is to illustrate that any number of individual services can be used to connect.
+This is to illustrate that any number of individual services can be used to connect. Also that the -Tenant parameter is positional
 
 ### -------------------------- EXAMPLE 3 --------------------------
 ```
@@ -310,6 +314,21 @@ Accept wildcard characters: False
 
 ```yaml
 Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: 
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -User
+{{Fill User Description}}
+
+```yaml
+Type: String
 Parameter Sets: (All)
 Aliases: 
 
